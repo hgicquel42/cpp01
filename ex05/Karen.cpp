aa5c39187d310c29137b5fef752a61efbada8af6
hgicquel <hgicquel@student.42.fr>
2022-01-24 14:07:58 +0100

    uppercase

--- ex05/Karen.cpp
+++ ex05/Karen.cpp
@@ -6,32 +6,32 @@
 /*   By: hgicquel <hgicquel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 17:40:32 by hgicquel          #+#    #+#             */
-/*   Updated: 2022/01/18 17:58:53 by hgicquel         ###   ########.fr       */
+/*   Updated: 2022/01/24 14:06:13 by hgicquel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Karen.hpp"
 #include <iostream>
 
-void	Karen::debug( void )
+void	Karen::debug(void)
 {
 	std::cout << "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger." << " ";
 	std::cout << "I just love it!" << "\n";
 }
 
-void	Karen::info( void )
+void	Karen::info(void)
 {
 	std::cout << "I cannot believe adding extra bacon cost more money." << " ";
 	std::cout << "You don’t put enough! If you did I would not have to ask for it!" << "\n";
 }
 
-void	Karen::warning( void )
+void	Karen::warning(void)
 {
 	std::cout << "I think I deserve to have some extra bacon for free." << " ";
 	std::cout << "I’ve been coming here for years and you just started working here last month." << "\n";
 }
 
-void	Karen::error( void )
+void	Karen::error(void)
 {
 	std::cout << "This is unacceptable, I want to speak to the manager now." << "\n";
 }
@@ -39,7 +39,7 @@ void	Karen::error( void )
 void	Karen::complain(std::string level)
 {
 	int	i;
-	std::string levels[] = { "debug", "info", "warning", "error" };
+	std::string levels[] = { "DEBUG", "INFO", "WARNING", "ERROR" };
 	void (Karen::*methods[])(void) = { &Karen::debug, &Karen::info, &Karen::warning, &Karen::error };
 
 	i = 0;
--- ex05/main.cpp
+++ ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hgicquel <hgicquel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 17:59:16 by hgicquel          #+#    #+#             */
-/*   Updated: 2022/01/18 18:00:22 by hgicquel         ###   ########.fr       */
+/*   Updated: 2022/01/24 14:06:37 by hgicquel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ int	main(void)
 {
 	Karen karen;
 
-	karen.complain("debug");
-	karen.complain("info");
-	karen.complain("warning");
-	karen.complain("error");
+	karen.complain("DEBUG");
+	karen.complain("INFO");
+	karen.complain("WARNING");
+	karen.complain("ERROR");
 	return (0);
 }
